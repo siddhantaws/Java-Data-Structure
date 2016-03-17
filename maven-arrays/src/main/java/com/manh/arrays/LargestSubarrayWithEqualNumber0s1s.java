12dad9a168bc7331ea8f196ee4ff316fae587f8e
Siddhanta Kumar Pattnaik <siddhantaws@gmail.com>
2016-03-17 22:30:17 +0530

    Largest subarray with equal number of 0s and 1s

--- maven-arrays/src/main/java/com/manh/arrays/LargestSubarrayWithEqualNumber0s1s.java
+++ maven-arrays/src/main/java/com/manh/arrays/LargestSubarrayWithEqualNumber0s1s.java
@@ -2,5 +2,24 @@ package com.manh.arrays;
 
 public class LargestSubarrayWithEqualNumber0s1s 
 {
+	private static int arr[]={1, 0, 0, 1, 0, 1, 1};
 	
+	public static void main(String[] args) 
+	{
+		int count0s= 0 ,count1s=0;
+		int matchCount =0 ,matchIndex =0;
+		for(int i=0;i<arr.length-1;i++)
+		{
+			if(arr[i]==0)
+				++count0s;
+			else
+				++count1s;
+			if(count0s == count1s )
+			{
+				matchCount = Math.max( matchCount, count0s*2);	
+				matchIndex = i;
+			}
+		}
+		System.out.println("Total Count "+matchCount +" end Index " +matchIndex +" Start Index ");
+	}
 }
