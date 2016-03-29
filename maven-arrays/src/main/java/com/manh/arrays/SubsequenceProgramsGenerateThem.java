dff87ab0f087524b07c915f68818fef58a21e2c3
siddhantaws <siddhantaws@gmail.com>
2016-03-29 11:50:40 +0530

    Programs to Generate Subsequence

--- maven-arrays/src/main/java/com/manh/arrays/SubsequenceProgramsGenerateThem.java
+++ maven-arrays/src/main/java/com/manh/arrays/SubsequenceProgramsGenerateThem.java
@@ -1,23 +1,25 @@
 package com.manh.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubsequenceProgramsGenerateThem 
 {
-	private static int arr[] = {1, 2, 3, 4};
-	
-	public static void main(String[] args) {
-		int opsize = (int) Math.pow(2, arr.length);
-		 
-	    /* Run from counter 000..1 to 111..1*/
-	    for (int counter = 1; counter < opsize; counter++)
-	    {
-	        for (int j = 0; j < arr.length; j++)
-	        {
-	            /* Check if jth bit in the counter is set
-	                If set then print jth element from arr[] */
-	            if (counter & (1<<j))
-	                cout << arr[j] << " ";
+	public static void generateSequence(String[] args) {
+	    List<String> results = new ArrayList<String>();
+	    for ( int i = 1; i <= (1<<(args.length))-1; i++ ) {
+	      StringBuilder builder = new StringBuilder();
+	      for ( int j = 0; j < args.length; j++ ) {
+	        if ( (i & (1<<j)) != 0) {
+	          builder.append(args[j]);
 	        }
-	      
-	    }	
+	      }
+	      results.add(builder.toString());
+	    }
+	    System.out.println( results );
+	  }
+	  
+	  public static void main(String[] args) {
+		  SubsequenceProgramsGenerateThem.generateSequence( new String[]{"1","2","3","4"});
 	}
 }
